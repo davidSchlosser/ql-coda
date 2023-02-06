51cc03e17a9b1e830f49688cecf080e98ac4ebbf
david <dls49_au@yahoo.com>
2023-02-06 19:57:02 +1100

    refactor, convert more to Riverpod

--- ql_coda/lib/main.dart
+++ ql_coda/lib/main.dart
@@ -1,27 +1,19 @@
 import 'package:coda/obsolete/saved_filters_bloc.dart';
 import 'package:coda/communicator.dart';
 import 'package:coda/logger.dart';
-import 'package:coda/models/current_track_model.dart';
-//import 'package:coda/obsolete/playlist_model.dart';
-import 'package:coda/models/clipboard_model.dart';
-import 'package:coda/models/control_panel_model.dart';
 import 'package:coda/models/cover_model.dart';
 import 'package:coda/models/volume_model.dart';
 import 'package:coda/models/query_model.dart';
 import 'package:coda/repositories/repositories.dart';
-//import 'package:coda/screens/current_track.dart';
-//import 'package:coda/screens/playlist_view.dart';
-//import 'package:coda/screens/cache_tags.dart';
-import 'package:coda/screens/common_scaffold.dart';
-import 'package:coda/streams/current_track_stream.dart';
+import 'package:coda/routing,dart.dart';
 import 'package:coda/streams/cover_stream.dart';
-import 'package:coda/streams/progress_stream.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:provider/provider.dart' as old_way;
+import 'package:provider/provider.dart' as old;
 import 'package:logger/logger.dart';
 
-//import 'models/albums_model.dart';
+import 'models/player_model.dart';
+
 
 Logger _logger = getLogger('main', Level.debug);
 
@@ -34,7 +26,10 @@ class CodaRP extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Coda());
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Coda()
+    );
   }
 
 }
@@ -55,38 +50,30 @@ class Coda extends StatelessWidget with WidgetsBindingObserver {
     WidgetsBinding.instance.addObserver(this);
   }
 
-  final CurrentTrackStream cts = CurrentTrackStream();
-  final CoverStream cs = CoverStream();
+  final Player _player = Player();
 
   @override
   Widget build(BuildContext context) {
     _logger.w('build');
-    return old_way.MultiProvider(
+    return old.MultiProvider(
       providers: [
-        old_way.StreamProvider<CurrentTrackModel>( // TODO migrate to RiverPod
+        /*old.StreamProvider<CurrentTrackModel>( // TODO migrate to RiverPod
           initialData: CurrentTrackModel(),
           create: (_) => CurrentTrackStream.currentTrackStreamController.stream
-        ),
-        old_way.StreamProvider<CoverModel>( // TODO migrate to RiverPod
+        ),*/
+        old.StreamProvider<CoverModel>( // TODO migrate to RiverPod
           initialData: CoverModel(),
           create: (_) => CoverStream.coverStreamController.stream
         ),
-        old_way.StreamProvider<Progress>( // TODO migrate to RiverPod
-          initialData: Progress(0.0),
-          create: (_) => ProgressStream.progressStreamController.stream
-        ),
-        old_way.StreamProvider<Volume>( // TODO migrate to RiverPod
+        old.StreamProvider<Volume>( // TODO migrate to RiverPod
           initialData: Volume(0),
           create: (_) => VolumeModel.volumeStreamController.stream
         ),
-        old_way.Provider<ControlPanelModel>(create: (_) => ControlPanelModel()), // TODO migrate to RiverPod
-        old_way.ChangeNotifierProvider(create: (context) => QueryModel()),  // TODO migrate to RiverPod
-        old_way.ChangeNotifierProvider(create: (context) => SavedQueriesModel()), // TODO migrate to RiverPod
-        /*old_way.ChangeNotifierProvider(
-            create: (context) => CheckedPlaylistTracks()),*/ // TODO migrate to RiverPod
+        old.ChangeNotifierProvider(create: (context) => QueryModel()),  // TODO migrate to RiverPod
+        old.ChangeNotifierProvider(create: (context) => SavedQueriesModel()), // TODO migrate to RiverPod
       ],
       child: MaterialApp.router(
-        title: 'Quodlibet Coda',
+        title: 'Coda',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
@@ -100,8 +87,7 @@ class Coda extends StatelessWidget with WidgetsBindingObserver {
   void didChangeAppLifecycleState(AppLifecycleState state) {
     _logger.d('$state');
     if (state == AppLifecycleState.resumed) {
-      //Communicator().reset();   // this causes issue if it occurs during tag cache rebuild
-      ControlPanelModel().refresh();
+      _player.refresh();
     }
   }
 }
