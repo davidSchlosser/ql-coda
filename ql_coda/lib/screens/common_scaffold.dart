b8a9ecef4ea755da10e2b8317723eaf66e098713
david <dls49_au@yahoo.com>
2023-02-12 10:06:31 +1100

    clean code

--- ql_coda/lib/screens/common_scaffold.dart
+++ ql_coda/lib/screens/common_scaffold.dart
@@ -1,77 +1,10 @@
-// import 'package:coda/logger.dart';
+import 'package:coda/logger.dart';
 import 'package:coda/screens/ui_util.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-/*
-import 'package:coda/screens/current_track.dart';
-import 'package:coda/screens/playlist_view.dart';
-import 'package:coda/screens/cache_tags.dart';
-import 'package:coda/screens/queries_view.dart';
-import 'package:coda/screens/queue_view.dart';
-import 'album_tracks_view.dart';
-import 'albums_view.dart';
-import 'edit_single_tag_view.dart';
-import 'track_tags_view.dart';
-*/
+import 'package:logger/logger.dart';
 
-/*
-GoRouter router() {
-  return GoRouter(
-    initialLocation: '/nowPlaying',
-    routes: [
-      GoRoute(
-        path: '/nowPlaying',
-        builder: (context, state) => CurrentTrack(),
-      ),
-      GoRoute(
-        path: '/playlist',
-        builder: (context, state) => const PlaylistView(),
-      ),
-      GoRoute(
-        path: '/cacheTags',
-        builder: (context, state) => const CachePage(),
-      ),
-      GoRoute(
-        path: '/queue',
-        builder: (context, state) => const QueueView(),
-      ),
-      GoRoute(
-        path: '/albums',
-        builder: (context, state) => const AlbumsView(),
-      ),
-      GoRoute(
-        path: '/albumtracks',
-        name: 'albumtracks',
-        builder: (context, state) => const AlbumTracksView(),
-      ),
-      GoRoute(
-        path: '/edittags',
-        builder: (context, state) => const EditTagsView(),
-      ),
-      GoRoute(
-        path: '/editSingleTag',
-        // builder: (context, state) => FamilyScreen(family: state.extra! as Family),
-        builder: (context, state) => EditSingleTagView(tagIndex: state.extra as int),
-      ),
-      GoRoute(
-        path: '/queries',
-        builder: (context, state) => QueriesView(),
-      ),
-      GoRoute(
-        path: '/speakers',
-        builder: (context, state) => const NilWidget(),
-      ),
-      GoRoute(
-        path: '/logs',
-        builder: (context, state) => const NilWidget(),
-      ),
-    ],
-  );
-}
-*/
-
-
-// Logger _logger = getLogger('main', Level.debug);
+Logger _logger = getLogger('main', Level.warning);
 
 class CommonScaffold extends StatelessWidget {
   final String title;
