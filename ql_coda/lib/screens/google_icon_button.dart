fa2af898480cffeaca5b27251ad64206265e9b40
david <dls49_au@yahoo.com>
2023-02-14 16:32:22 +1100

    search button

--- ql_coda/lib/screens/google_icon_button.dart
+++ ql_coda/lib/screens/google_icon_button.dart
@@ -1,28 +1,30 @@
-//import 'package:coda/blocs/blocs.dart';
 import 'package:coda/logger.dart';
 import 'package:coda/models/tags_model.dart';
-import 'package:coda/screens/google_search.dart';
+//import 'package:coda/screens/google_search.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:logger/logger.dart';
+import 'package:coda/models/edited_tags_model.dart';
 
 Logger _logger = getLogger('GoogleIconButton', Level.debug);
 
-class GoogleIconButton extends StatelessWidget {
-  const GoogleIconButton({super.key});
+class GoogleIconButton extends ConsumerWidget {
+
+  GoogleIconButton({super.key});
+
 
   @override
-  Widget build(BuildContext context) {
-    return IconButton(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final List<Tag> tags = ref.read(editedTagsProvider);
+    return ElevatedButton.icon(
+      label: const Text('Search web'),
       icon: const FaIcon(FontAwesomeIcons.google),
       onPressed: () {
         String artists = '';
         String title = '';
 
-        TagsState state = BlocProvider.of<TagsBloc>(context).state;
-        if (state is TagsLoaded) {
-          List<Tag> tags = state.tags.tags;
+        if (tags.isNotEmpty) {
           title = tags.firstWhere((test) {return test.name == 'album';}).value;
           List<Tag> artistsList = tags.where((test) {return test.name == 'artist';}).toList();
           artists = artistsList.expand((tag){return [tag.value];}).toList().join(',');
@@ -34,14 +36,14 @@ class GoogleIconButton extends StatelessWidget {
           _logger.d('not TagsLoaded');
         }
 
-        Navigator.push(
+        /*Navigator.push(
           context,
           MaterialPageRoute(
               builder: (context) => GoogleSearch(
                 artists: artists,
                 title: title,
               )), //EditPage(Player.currentTrackFile)),
-        );
+        );*/
       },
     );
   }
